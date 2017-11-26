694fb0ce4db60cc6bf043c84b1d76cc6884f5924
Simone Galimberti <s.galimberti32@campus.unimib.it>
2017-11-26 15:41:48 +0100

    add the quickstart project after first maven's build

---
+++ quickstart/src/main/java/it/unimib/quickstart/App.java
@@ -0,0 +1,13 @@
+package it.unimib.quickstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
